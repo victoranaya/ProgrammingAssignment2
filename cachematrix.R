6e79a980185fd2741d3b571c6ad3df36619d1c41
victoranaya <victor_anaya@enesmorelia.unam.mx>
2020-06-05 16:45:28 -0500

    Second commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,17 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function or not
+## This function will cache the calculation of the inverse of a square 2 x 2
+## matrix assuming there is an inverse. The  function calculates the inverse
+## only once and keeps the result cached for further use. 
 
 makeCacheMatrix <- function(x = matrix()) {
-##do something
+
+        
+        
+        
+        
+        
 }
 
 
